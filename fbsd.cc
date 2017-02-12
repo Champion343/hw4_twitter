59bf1d25d47403425adfe0347c9f44d9c53a5090
jr-rgt <jr_rgt@tamu.edu>
2017-02-11 18:22:38 -0600

    Update fbsd.cc

--- fbsd.cc
+++ fbsd.cc
@@ -16,7 +16,7 @@ using fbp::Message;
 using fbp::CRMasterServer;
 using namespace std;
 
-//find exsiting name
+//find exsiting name in a vector, get index
 int findName(string username, vector<string>* list)
 {
 	for(int i=0; i < (int)list->size(); i++)
@@ -27,14 +27,24 @@ int findName(string username, vector<string>* list)
 
 struct Room
 {
-	//name of room and followers
+	//name of room, followers, and following
 	string username;
 	vector<string> followers;
+	vector<string> following;
 	
 	Room(string name) : username(name) {}
 	
-	//new friend
-	bool join(string follower)
+	//new following
+	bool addFriend(string person)
+	{
+		if(findName(person, &following) >= 0)
+			return false;
+		following.push_back(person);
+		return true;
+	}
+	
+	//new follower
+	bool addFollower(string follower)
 	{
 		if(findName(follower, &followers) >= 0)
 			return false;
@@ -42,10 +52,20 @@ struct Room
 		return true;
 	}
 	
-	//unfriended
-	bool leave(string follower)
+	//unfollow
+	bool unfriend(string person)
 	{
-		int index = findName(follower, &followers);
+		int index = findName(person, &following);
+		if( index < 0)
+			return false;
+		following.erase(following.begin()+index-1);
+		return true;
+	}
+	
+	//lost a follower
+	bool unfollowedBy(string person)
+	{
+		int index = findName(person, &followers);
 		if( index < 0)
 			return false;
 		followers.erase(followers.begin()+index-1);
@@ -56,7 +76,7 @@ struct Room
 //Global: all chatrooms
 vector<Room> chatRooms;
 
-//find exsiting chatroom
+//find exsiting chatroom, return index
 int findName(string username, vector<Room>* list)
 {
 	for(int i=0; i < (int)list->size(); i++)
@@ -72,54 +92,86 @@ bool createChatroom(string username)
 		return false;
 	Room newRoom(username);
 	chatRooms.push_back(newRoom);
-	cout << "created room " << username << endl;
+	cout << "created room: " << username << endl;
 	return true;
 }
 
 //overrides of proto
-class FBServiceImpl final : public CRMasterServer::Service {
+class FBServiceImpl final : public CRMasterServer::Service 
+{
+	
 	// Login
-  Status Login(ServerContext* context, const Message* request,
-                  Reply* reply) override {
-    if(!createChatroom(request->username()))
-		cout << "service no" << endl;
-	else
-		cout << "service yes" << endl;
-	reply->set_msg("doine");
-    return Status::OK;
-  }
+    Status Login(ServerContext* context, const Message* request, Reply* reply) 
+	override 
+	{
+		if(createChatroom(request->username()))
+			reply->set_msg("server created room");
+		else
+			reply->set_msg("server no created room");
+		return Status::OK;	
+	}
   
-  // List
-  Status List(ServerContext* context, const Message* request,
-                  ListReply* reply) override {
-    //getList(request->username);
-    return Status::OK;
-  }
+	// List
+	Status List(ServerContext* context, const Message* request, ListReply* reply) 
+	override 
+	{
+		//set all rooms
+		for(int i=0; i < (int)chatRooms.size(); i++)
+			reply->set_all_roomes(i, chatRooms[i].username);
+		//set all rooms joined by user
+		int index = findName(request->username(), &chatRooms);
+		for(int i=0; i < (int)chatRooms[index].following.size(); i++)
+			reply->set_joined_roomes(i, chatRooms[index].following[i]);
+		return Status::OK;
+	}
 
-// Join
-  Status Join(ServerContext* context, const Message* request,
-                  Reply* reply) override {
-    //joinChatroom(request->username, request->arguments.(0));
-    return Status::OK;
-  }
+	// Join
+	Status Join(ServerContext* context, const Message* request, Reply* reply) 
+	override 
+	{
+		//add new friend to user, add user to new friend's followers
+		int user, joining;
+		user = findName(request->username(), &chatRooms);
+		joining = findName(request->msg(), &chatRooms);
+		if( user < 0 || joining < 0)
+			reply->set_msg("join fail");
+		else if( chatRooms[user].addFriend(request->msg()) &&
+				 chatRooms[joining].addFollower(request->username()) )
+					reply->set_msg("join success");
+		else
+			reply->set_msg("join fail");
+		return Status::OK;
+	}
 
-// Leave
-  Status Leave(ServerContext* context, const Message* request,
-                  Reply* reply) override {
-    //leaveChatroom(request->username, request->arguments.(0));
-    return Status::OK;
-  }
+	// Leave
+	Status Leave(ServerContext* context, const Message* request, Reply* reply) 
+	override 
+	{
+		//delete person from user's friends, delete user from person's followers
+		int user, leaving;
+		user = findName(request->username(), &chatRooms);
+		leaving = findName(request->msg(), &chatRooms);
+		if( user < 0 || leaving < 0)
+			reply->set_msg("leave fail");
+		else if( chatRooms[user].unfriend(request->msg()) &&
+				 chatRooms[leaving].unfollowedBy(request->username()) )
+					reply->set_msg("leave success");
+		else
+			reply->set_msg("leave fail");
+		return Status::OK;
+	}
 
-// Chat
-  Status Chat(ServerContext* context, const Message* msg,
-                  Message* reply) {
-    //bistreamMsg(request->username, request->arguments.(0));
-    return Status::OK;
-  }
+	// Chat
+	Status Chat(ServerContext* context, const Message* msg, Message* reply) 
+	{
+		//bistreamMsg(request->username, request->arguments.(0));
+		return Status::OK;
+	}
   
 };
 
-void RunServer() {
+void RunServer() 
+{
   string server_address("0.0.0.0:50081");
   FBServiceImpl service;
 
@@ -138,7 +190,8 @@ void RunServer() {
   server->Wait();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
   RunServer();
 
   return 0;
