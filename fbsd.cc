2084966f1398c20ab35a42bbb2a3c46ee0dbabe9
jr-rgt <jr_rgt@tamu.edu>
2017-04-11 20:38:14 -0500

    ayy

--- fbsd.cc
+++ fbsd.cc
@@ -13,13 +13,6 @@
 #include <thread>
 #include "fbp.grpc.pb.h"
 
-#include <thread>
-#include <grpc++/grpc++.h>
-#include <unistd.h>
-#include <google/protobuf/util/time_util.h>
-#include <google/protobuf/timestamp.pb.h>
-#include <google/protobuf/duration.pb.h>
-
 using google::protobuf::Timestamp;
 using google::protobuf::Duration;
 using grpc::Server;
@@ -32,172 +25,7 @@ using fbp::Reply;
 using fbp::ListReply;
 using fbp::Message;
 using fbp::CRMasterServer;
-
-using grpc::Channel;
-using grpc::ClientReaderWriter;
-using grpc::ClientContext;
-
 using namespace std;
-
-//Helper function used to create a Message object given a username and message
-Message MakeMessage(const std::string& username, const std::string& msg) {
-  Message m;
-  m.set_username(username);
-  m.set_msg(msg);
-  google::protobuf::Timestamp* timestamp = new google::protobuf::Timestamp();
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  timestamp->set_seconds(tv.tv_sec);
-  timestamp->set_nanos(tv.tv_usec * 1000);
-  //timestamp->set_seconds(time(NULL));
-  //timestamp->set_nanos(0);
-  m.set_allocated_timestamp(timestamp);
-  google::protobuf::int64 t1; 
-	t1 = google::protobuf::util::TimeUtil::TimestampToNanoseconds(*timestamp);
-	cout << " timestamp " << t1 << endl;
-  return m;
-}
-
-//thread handles reading for bidirectional streaming
-void reading(shared_ptr<ClientReaderWriter<Message,Message>> stream)
-{
-	Message server_message;
-	while(stream->Read(&server_message)) {//blocking;
-		cout << server_message.username()<<": " <<server_message.msg() << endl;
-		
-	}
-}
-//Client object used for grpc calls
-class Client {
- public:
-  Client(std::shared_ptr<Channel> channel)
-      : stub_(CRMasterServer::NewStub(channel)) {}
-
-  //Login function send message containing username and receives response
-  std::string Login(const std::string& user) {
-    // Data we are sending to the server.
-    Request message;
-    message.set_username(user);
-    // Container for the data we expect from the server.
-    Reply reply;
-    ClientContext context;
-
-    // The actual RPC.
-    Status status = stub_->Login(&context, message, &reply);
-
-    // Act upon its status.
-    if (status.ok()) {
-      return reply.msg();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
-    }
-  }
-	//Join function sends a message containing username and the room that is being
-	//joined then returns server response
-    std::string Join(const std::string& user, std::string& room) {
-    // Data we are sending to the server.
-    Request message;
-    message.set_username(user);
-	message.add_arguments(room);
-    // Container for the data we expect from the server.
-    Reply reply;
-
-    ClientContext context;
-
-    // The actual RPC.
-    Status status = stub_->Join(&context, message, &reply);
-
-    // Act upon its status.
-    if (status.ok()) {
-      return reply.msg();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
-    }
-  }
-    //Leave function sends a message containing username and the room that is being
-	//left then returns server response
-    std::string Leave(const std::string& user, std::string& room) {
-    // Data we are sending to the server.
-    Request message;
-    message.set_username(user);
-	message.add_arguments(room);
-    // Container for the data we expect from the server.
-    Reply reply;
-
-    ClientContext context;
-
-    // The actual RPC.
-    Status status = stub_->Leave(&context, message, &reply);
-
-    // Act upon its status.
-    if (status.ok()) {
-      return reply.msg();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
-    }
-  }
-    
-    ListReply List(const std::string& user) {
-    // Data we are sending to the server.
-    Request message;
-    message.set_username(user);
-    // Container for the data we expect from the server.
-    ListReply reply;
-
-    ClientContext context;
-
-    // The actual RPC.
-    Status status = stub_->List(&context, message, &reply);
-
-    // Act upon its status.
-    if (status.ok()) {
-      return reply;
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << "RPC failed" << std::endl;
-      return reply;
-    }
-  }
-  //Chat function opens a bidirectional stream to the server then sends
-  //its username and begins reading and writing to the server
-  void Chat(const std::string& user){
-	ClientContext context;
-	//bidirectional streaming
-	std::shared_ptr<ClientReaderWriter<Message,Message>> stream(stub_->Chat(&context));
-	string text;
-	Message client_message;
-	//reading thread
-	thread readMsg(reading, stream);
-	cout << "Begin Chatting..." << endl;
-	//remove anything left over from the command line
-	client_message = MakeMessage(user, "");
-	//send initial message declaring username
-	stream->Write(client_message);
-	//loop through requesting user input and send message to server
-	unsigned int microseconds;
-	string input;
-	cin >> input;//wait before entering for loop
-	//here we send 50 messages
-	for(int i = 0; i <50; i++){
-	microseconds = 500;//time interval
-	
-	usleep(microseconds);
-    client_message = MakeMessage(user, "message");
-	stream->Write(client_message);
-	}
-	//if we ever wanted an exit this would close the stream and exit Chat
-	Status status = stream->Finish();
-}
-  
- private:
-  std::unique_ptr<CRMasterServer::Stub> stub_;
-};
 
 //find exsiting name in a vector, get index
 int findName(string username, vector<string>* list)
@@ -374,18 +202,6 @@ class FBServiceImpl final : public CRMasterServer::Service
 	
 	bool first = true;
 	int last = 0;
-	
-	// Connect
-    Status Connect(ServerContext* context, const Request* request, Reply* reply) 
-	override 
-	{
-		cout << "assigning worker: " << request->username() << endl;
-		if(createChatroom(request->username()))
-			reply->set_msg("server assigned worker");
-		else
-			reply->set_msg("server no assigned worker");
-		return Status::OK;	
-	}
 	
 	// Login
     Status Login(ServerContext* context, const Request* request, Reply* reply) 
@@ -643,28 +459,6 @@ int main(int argc, char** argv)
   {
 	cout << "default port 0.0.0.0:50032" << endl;
   }
-  //extra agruement to determine replica?
   RunServer(server_address);
-  
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint (in this case,
-  // localhost at port 50051). We indicate that the channel isn't authenticated
-  // (use of InsecureChannelCredentials()).
-  ListReply lreply;
-  string client_name;
-  string input;
-  string room_name;
-  string host_name = "";
-  string port = "";
-  host_name.append(":");
-  host_name.append(port);
-  //sprintf("%s:%d",host_name,port);
-  client_name = "";
-  //create connection to server
-  Client client(grpc::CreateChannel(
-      host_name, grpc::InsecureChannelCredentials()));
-  std::string reply = client.Login(client_name);
-  std::cout << "Login State: " << reply << std::endl;
-  
   return 0;
 }
